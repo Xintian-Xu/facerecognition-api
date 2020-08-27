cc672b414f8110f2903e89d9fad5e8065d45617e
Xintian Xu <xintianxu@Xintian-Xu.local>
2020-08-26 21:50:12 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const app = express();
+
+const signin = require('./controllers/signin');
+const signup = require('./controllers/signup');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+	    user : 'postgres',
+	    password : 'postgres',
+	    database : 'facerecognition'
+	  }
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send(database.users) });
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/signup', signup.handleSignup(db, bcrypt));
+app.get('/profile/:id', profile.handleProfile(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+
+
+
+
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
+});
+
+
+
